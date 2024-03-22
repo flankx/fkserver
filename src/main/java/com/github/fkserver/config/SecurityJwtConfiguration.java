2b0b6565f7fe109048ba52dddd0264c0ea1fc1d5
xujianrong <xujianrong@leateckgroup.com>
2024-03-22 13:45:55 +0800

    add SecurityMetersService

--- src/main/java/com/github/fkserver/config/SecurityJwtConfiguration.java
+++ src/main/java/com/github/fkserver/config/SecurityJwtConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 
+import com.github.fkserver.management.SecurityMetersService;
 import com.github.fkserver.security.SecurityUtils;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.nimbusds.jose.util.Base64;
@@ -28,8 +29,25 @@ public class SecurityJwtConfiguration {
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(SecurityUtils.JWT_ALGORITHM).build();
+    public JwtDecoder jwtDecoder(SecurityMetersService metersService) {
+        NimbusJwtDecoder jwtDecoder =
+            NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(SecurityUtils.JWT_ALGORITHM).build();
+        return token -> {
+            try {
+                return jwtDecoder.decode(token);
+            } catch (Exception e) {
+                if (e.getMessage().contains("Invalid signature")) {
+                    metersService.trackTokenInvalidSignature();
+                } else if (e.getMessage().contains("Jwt expired at")) {
+                    metersService.trackTokenExpired();
+                } else if (e.getMessage().contains("Invalid JWT serialization")) {
+                    metersService.trackTokenMalformed();
+                } else if (e.getMessage().contains("Invalid unsecured/JWS/JWE")) {
+                    metersService.trackTokenMalformed();
+                }
+                throw e;
+            }
+        };
     }
 
     @Bean
